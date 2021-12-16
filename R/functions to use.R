0316eb0debdea81f23925793a6746f653dcbdce8
Yangshen0325 <sylviadyx77@163.com>
2021-12-16 20:25:41 +0100

    modified if a species has no partners on island,so the denominator is 0

--- R/functions to use.R
+++ R/functions to use.R
@@ -1,6 +1,6 @@
 # get mutualistic partners and competitors for animal and plant species
 # Mt <- {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)}
-# p_status<-c(1,1,0,1)
+# p_status<-c(0,1,0,0)
 # a_status<-c(1,1,1,0,1)
 # get_part_compe(Mt, p_status,a_status)
 
@@ -18,16 +18,12 @@ get_part_compe <- function(Mt,
   for (x in seq(ncol(tMt))){
     plant_compe[x] <- sum((colSums(tMt[,x]*tMt[,-x]* a_status)>=1) * as.matrix(p_status)[-x,])
   }  
-  plant_compe[which(p_status==0)] <- 0
-  # the number of competitors of each plant species, elements equal to 0 means no competitors or
-  # the species is not shown up in island
+  # the number of competitors of each plant species
   
   for (x in seq(ncol(Mt))){
     animal_compe[x] <- sum((colSums(Mt[,x] * Mt[,-x]* p_status)>=1) * as.matrix(a_status)[-x,])
   } 
-  animal_compe[which(a_status==0)] <- 0
-  # the number of competitors of each animal species,elements equal to 0 means no competitors or
-  #the species is not shown up in island
+  # the number of competitors of each animal species
   
   part_compe_list <- list(plant_part = plant_part,
                           animal_part = animal_part,
@@ -43,6 +39,7 @@ get_part_compe <- function(Mt,
 # KP1: a coefficient showing the influence from mutualism to plant species
 # KA1: a coefficient showing the influence from mutualism to animal species
 # K_par <- c(20,0.6,20,0.6)
+# get_NK(K_par,Mt, p_status,a_status)
 get_NK <- function(K_par,
                    Mt,
                    p_status,
@@ -51,14 +48,19 @@ get_NK <- function(K_par,
   part_compe_list <- get_part_compe(Mt=Mt,
                                     p_status= p_status,
                                     a_status= a_status)
- 
-  part_compe_list[[1]][-which(part_compe_list[[1]]==0)]
-  part_compe_list[[2]][-which(part_compe_list[[2]]==0)]
   
-  plant_NK <-  exp(-(sum(p_status)/K_par[1])+
-                     part_compe_list[[3]]/(K_par[2]*part_compe_list[[1]]))
-  animal_NK <- exp(-(sum(a_status)/K_par[3])+
-                     part_compe_list[[4]]/(K_par[4]*part_compe_list[[2]]))
+  indp <- which(part_compe_list[[1]]==0)
+  inda <- which(part_compe_list[[2]]==0)
+  
+      plant_NK <- exp(-(sum(p_status)/K_par[1])+
+                           part_compe_list[[3]]/(K_par[2]*part_compe_list[[1]]))
+      plant_NK[indp] <- exp(-(sum(p_status)/K_par[1]))
+ 
+ 
+      animal_NK <- exp(-(sum(a_status)/K_par[3])+
+                            part_compe_list[[4]]/(K_par[4]*part_compe_list[[2]])) 
+      animal_NK[inda] <- exp(-(sum(a_status)/K_par[3]))
+
   
   NK_list <- list(plant_NK = plant_NK,
                   animal_NK = animal_NK)
