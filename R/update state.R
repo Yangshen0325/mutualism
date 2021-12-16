61f8ef7433a04d273702d963709d935a4d838af2
Yangshen0325 <sylviadyx77@163.com>
2021-12-17 00:01:44 +0100

    modified to cladogenesis on plant species

--- R/update state.R
+++ R/update state.R
@@ -123,7 +123,9 @@ update_state <- function(
   p_status,
   a_status){
   
-  if(possible_event$L1 == 1){# [1]: immigration event with plant species
+  ##########################################
+  # [1]: immigration event with plant species
+  if(possible_event$L1 == 1){
     
     colonist = possible_event$Var1
     p_status[colonist] <- 1
@@ -136,198 +138,197 @@ update_state <- function(
     {
       island_spec = rbind(island_spec,c(colonist,colonist,timeval,"I",NA,NA,NA,"p"))
     }
+  } 
+  ##########################################
+  # [2]: extinction event with plant species
+  if(possible_event$L1 == 2){
+    
+    extinct =  possible_event$Var1
+    p_status[extinct] <- 0
+    
+    typeofspecies = island_spec[extinct,4]
+    
+    if(typeofspecies == "I")
+    {
+      island_spec = island_spec[-extinct,] #remove immigrant
+    }
+    
+    if(typeofspecies == "A")
+    {
+      island_spec = island_spec[-extinct,] #remove anagenetic
+    }
+    
+    if(typeofspecies == "C")
+    {
+      #remove cladogenetic
+      #first find species with same ancestor AND arrival totaltime
+      sisters = intersect(which(island_spec[,2] == island_spec[extinct,2]),which(island_spec[,3] == island_spec[extinct,3]))
+      survivors = sisters[which(sisters != extinct)]
+      
+      if(length(sisters) == 2)
+      {
+        #survivors status becomes anagenetic
+        island_spec[survivors,4] = "A"
+        island_spec[survivors,c(5,6)] = c(NA,NA)
+        island_spec[survivors,7] = "Clado_extinct"
+        island_spec = island_spec[-extinct, ]
+      }
+      
+      if(length(sisters) >= 3)
+      {
+        numberofsplits = nchar(island_spec[extinct,5])
+        mostrecentspl = substring(island_spec[extinct,5],numberofsplits)
+        
+        if(mostrecentspl=="B")
+        {
+          sistermostrecentspl = "A"
+        }
+        if(mostrecentspl=="A")
+        {
+          sistermostrecentspl = "B"
+        }
+        
+        motiftofind = paste(substring(island_spec[extinct,5],1,numberofsplits-1),sistermostrecentspl,sep = "")
+        possiblesister = survivors[which(substring(island_spec[survivors,5],1,numberofsplits) == motiftofind)]
+        
+        if(mostrecentspl == "A")
+        {
+          #change the splitting date of the sister species so that it inherits the early splitting that used to belong to A.
+          
+          tochange = possiblesister[which(island_spec[possiblesister,6] == min(as.numeric(island_spec[possiblesister,6])))]
+          island_spec[tochange,6] = island_spec[extinct,6]
+        }
+        #remove the offending A/B from these species
+        island_spec[possiblesister,5] = paste(substring(island_spec[possiblesister,5],1,numberofsplits - 1),
+                                              substring(island_spec[possiblesister,5],numberofsplits + 1,
+                                                        nchar(island_spec[possiblesister,5])),sep = "")
+        island_spec = island_spec[-extinct, ]
+      }
+    }
+    island_spec = rbind(island_spec)
+  }
+  ##########################################
+  # [3]: cladogenesis event with plant species
+  if(possible_event$L1 == 3){
+    
+    tosplit = possible_event$Var1
+    
+    Mt <- new_Mt_clado_p(Mt=Mt,possible_event=possible_event,p=p)
+    p_status[tosplit] <- 0
+    p_status <- c(p_status,1,1)
+    
+    if(island_spec[tosplit,4] == "C")
+    {
+      #for daughter A
+      island_spec[tosplit,4] = "C"
+      island_spec[tosplit,1] = maxspecID + 1
+      oldstatus = island_spec[tosplit,5]
+      island_spec[tosplit,5] = paste(oldstatus,"A",sep = "")
+      #island_spec[tosplit,6] = timeval
+      island_spec[tosplit,7] = NA
+      island_spec[tosplit,8] = "1"
+      
+      #for daughter B
+      island_spec = rbind(island_spec,c(maxspecID + 2,island_spec[tosplit,2],island_spec[tosplit,3],
+                                        "C",paste(oldstatus,"B",sep = ""),timeval,NA,1))
+      maxspecID = maxspecID + 2
+    } else {
+      #if the species that speciates is not cladogenetic
+      
+      #for daughter A
+      
+      island_spec[tosplit,4] = "C"
+      island_spec[tosplit,1] = maxspecID + 1
+      island_spec[tosplit,5] = "A"
+      island_spec[tosplit,6] = island_spec[tosplit,3]
+      island_spec[tosplit,7] = NA
+      island_spec[tosplit,8] = "1"
+      
+      #for daughter B
+      island_spec = rbind(island_spec,c(maxspecID + 2,island_spec[tosplit,2],island_spec[tosplit,3],"C","B",timeval,NA,1))
+      maxspecID = maxspecID + 2
+    }
+  } 
+  ##########################################
+  # [4]: anagenesis event with plant species
+  if(possible_event$L1 == 4){
+    
+    anagenesis = possible_event$Var1
+    
+    Mt <- new_Mt_ana_p(Mt=Mt,possible_event,p=p)
+    p_status[anagenesis] <- 0
+    p_status <- c(p_status,1)
+    
+    maxspecID = maxspecID + 1
+    island_spec[anagenesis,4] = "A"
+    island_spec[anagenesis,1] = maxspecID
+    island_spec[anagenesis,7] = "Immig_parent"
+    island_spec[anagenesis,8] = "p"
     
   } else
-    if(possible_event$L1 == 2){# [2]: extinction event with plant species
+    if(possible_event$L1 == 5){# [5]: immigration event with animal species
       
-      island_spec_plant = which(island_spec[,8] == "p")
-      extinctp = DDD::sample2(island_spec_plant,1)
-      p_status[extinctp] <- 0
+      Mt <- Mt # can be deleted
+      h <- event$Var1
+      p_status <- p_status # can be deleted
+      a_status[h] <- 1
       
-      typeofspecies = island_spec[extinctp,4]
-      
-      if(typeofspecies == "I")
-      {
-        island_spec = island_spec[-extinctp,] #remove immigrant
-      }
-      
-      if(typeofspecies == "A")
-      {
-        island_spec = island_spec[-extinctp,] #remove anagenetic
-      }
-      
-      if(typeofspecies == "C")
-      {
-        #remove cladogenetic
-        #first find species with same ancestor AND arrival totaltime
-        sisters = intersect(which(island_spec[,2] == island_spec[extinctp,2]),which(island_spec[,3] == island_spec[extinctp,3]))
-        survivors = sisters[which(sisters != extinctp)]
+    } else
+      if(event$L1 == 6){# [6]: extinction event with animal species
         
-        if(length(sisters) == 2)
-        {
-          #survivors status becomes anagenetic
-          island_spec[survivors,4] = "A"
-          island_spec[survivors,c(5,6)] = c(NA,NA)
-          island_spec[survivors,7] = "Clado_extinct"
-          island_spec = island_spec[-extinctp,]
-        }
+        Mt <- Mt # can be deleted
+        h <- event$Var1
+        p_status <- p_status # can be deleted
+        a_status[h] <- 0
         
-        if(length(sisters) >= 3)
-        {
-          numberofsplits = nchar(island_spec[extinctp,5])
-          mostrecentspl = substring(island_spec[extinct,5],numberofsplits)
+      } else 
+        if(event$L1 == 7){# [7]: cladogenesis event with animal species
           
-          if(mostrecentspl=="B")
-          {
-            sistermostrecentspl = "A"
-          }
-          if(mostrecentspl=="A")
-          {
-            sistermostrecentspl = "B"
-          }
-          
-          motiftofind = paste(substring(island_spec[extinct,5],1,numberofsplits-1),sistermostrecentspl,sep = "")
-          possiblesister = survivors[which(substring(island_spec[survivors,5],1,numberofsplits) == motiftofind)]
-          
-          if(mostrecentspl == "A")
-          {
-            #change the splitting date of the sister species so that it inherits the early splitting that used to belong to A.
-            tochange = possiblesister[which(island_spec[possiblesister,6] == min(as.numeric(island_spec[possiblesister,6])))]
-            island_spec[tochange,6] = island_spec[extinctp,6]
-          }
-          
-          #remove the offending A/B from these species
-          island_spec[possiblesister,5] = paste(substring(island_spec[possiblesister,5],1,numberofsplits - 1),
-                                                substring(island_spec[possiblesister,5],numberofsplits + 1,
-                                                          nchar(island_spec[possiblesister,5])),sep = "")
-          island_spec = island_spec[-extinctp,]
-        }
-      }
-      island_spec = rbind(island_spec)
-    }else
-      
-      if(possible_event$L1 == 3){# [3]: cladogenesis event with plant species
-        
-        island_spec_plant = which(island_spec[,8] == "p")
-        tosplitp = DDD::sample2(island_spec_plant,1)
-        
-        Mt <- new_Mt_clado_p(Mt=Mt,possible_event=possible_event,p=p)
-        p_status[tosplitp] <- 0
-        p_status <- c(p_status,1,1)
-        
-        if(island_spec[tosplitp,4] == "C")
-        {
-          #for daughter A
-          
-          island_spec[tosplitp,4] = "C"
-          island_spec[tosplitp,1] = maxspecID + 1
-          oldstatus = island_spec[tosplitp,5]
-          island_spec[tosplitp,5] = paste(oldstatus,"A",sep = "")
-          #island_spec[tosplit,6] = timeval
-          island_spec[tosplitp,7] = NA
-          island_spec[tosplitp,8] = "1"
-          
-          #for daughter B
-          island_spec = rbind(island_spec,c(maxspecID + 2,island_spec[tosplitp,2],island_spec[tosplitp,3],
-                                            "C",paste(oldstatus,"B",sep = ""),timeval,NA,1))
-          maxspecID = maxspecID + 2
-        } else {
-          #if the species that speciates is not cladogenetic
-          
-          #for daughter A
-          
-          island_spec[tosplitp,4] = "C"
-          island_spec[tosplitp,1] = maxspecID + 1
-          island_spec[tosplitp,5] = "A"
-          island_spec[tosplitp,6] = island_spec[tosplitp,3]
-          island_spec[tosplitp,7] = NA
-          island_spec[tosplitp,8] = "1"
-          
-          #for daughter B
-          island_spec = rbind(island_spec,c(maxspecID + 2,island_spec[tosplitp,2],island_spec[tosplitp,3],"C","B",timeval,NA,1))
-          maxspecID = maxspecID + 2
-        }
-        
-      } else
-        if(possible_event$L1 == 4){# [4]: anagenesis event with plant species
-          
-          immi_specs = intersect(which(island_spec[,4] == "I"), which(island_spec[,8] == "p"))
-          anagenesisp = DDD::sample2(immi_specs,1)
-          
-          Mt <- new_Mt_ana_p(Mt=Mt,possible_event,p=p)
-          p_status[anagenesisp] <- 0
-          p_status <- c(p_status,1)
-          
-          maxspecID = maxspecID + 1
-          island_spec[anagenesisp,4] = "A"
-          island_spec[anagenesisp,1] = maxspecID
-          island_spec[anagenesisp,7] = "Immig_parent"
-          island_spec[anagenesisp,8] = "p"
+          Mt <- new_Mt_clado_a(Mt=Mt,event=event,p=p)
+          p_status <- p_status # can be deleted
+          h <- event$Var1
+          a_status[h] <- 0
+          a_status <- c(a_status,1,1)
           
         } else
-          if(possible_event$L1 == 5){# [5]: immigration event with animal species
+          if(event$L1 == 8){# [8]: anagenesis event with animal species
             
-            Mt <- Mt # can be deleted
-            h <- event$Var1
+            Mt <- new_Mt_ana_a(Mt=Mt,event=event,p=p)
             p_status <- p_status # can be deleted
-            a_status[h] <- 1
+            h <- event$Var1
+            a_status[h] <- 0
+            a_status <- c(a_status,1)
             
-          } else
-            if(event$L1 == 6){# [6]: extinction event with animal species
+          } else 
+            if(event$L1 == 9){# [9]: cospeciation event between pairs
               
-              Mt <- Mt # can be deleted
+              Mt <- new_Mt_cospec(Mt=Mt,event=event,p=p)
               h <- event$Var1
-              p_status <- p_status # can be deleted
-              a_status[h] <- 0
+              k <- event$Var2
+              p_status[h] <- 0
+              a_status[k] <- 0
+              p_status <- c(p_status,1,1)
+              a_status <- c(a_status,1,1)
               
             } else 
-              if(event$L1 == 7){# [7]: cladogenesis event with animal species
+              if(event$L1 == 10){# [10]: gain links event between pairs
                 
-                Mt <- new_Mt_clado_a(Mt=Mt,event=event,p=p)
-                p_status <- p_status # can be deleted
                 h <- event$Var1
-                a_status[h] <- 0
-                a_status <- c(a_status,1,1)
+                k <- event$Var2
+                Mt[h,k] == 1
+                p_status <- p_status # can be deleted
+                a_status <- a_status # can be deleted
                 
-              } else
-                if(event$L1 == 8){# [8]: anagenesis event with animal species
+              } else 
+                if(event$L1 == 11){# [11]: loss links event between pairs
                   
-                  Mt <- new_Mt_ana_a(Mt=Mt,event=event,p=p)
-                  p_status <- p_status # can be deleted
                   h <- event$Var1
-                  a_status[h] <- 0
-                  a_status <- c(a_status,1)
+                  k <- event$Var2
+                  Mt[h,k] == 1
+                  p_status <- p_status # can be deleted
+                  a_status <- a_status # can be deleted
                   
-                } else 
-                  if(event$L1 == 9){# [9]: cospeciation event between pairs
-                    
-                    Mt <- new_Mt_cospec(Mt=Mt,event=event,p=p)
-                    h <- event$Var1
-                    k <- event$Var2
-                    p_status[h] <- 0
-                    a_status[k] <- 0
-                    p_status <- c(p_status,1,1)
-                    a_status <- c(a_status,1,1)
-                    
-                  } else 
-                    if(event$L1 == 10){# [10]: gain links event between pairs
-                      
-                      h <- event$Var1
-                      k <- event$Var2
-                      Mt[h,k] == 1
-                      p_status <- p_status # can be deleted
-                      a_status <- a_status # can be deleted
-                      
-                    } else 
-                      if(event$L1 == 11){# [11]: loss links event between pairs
-                        
-                        h <- event$Var1
-                        k <- event$Var2
-                        Mt[h,k] == 1
-                        p_status <- p_status # can be deleted
-                        a_status <- a_status # can be deleted
-                        
-                      }
+                }
   new_state <- list(Mt = Mt,
                     p_status = p_status,
                     a_status = a_status)
