10a0c64bb1d7bb26d623eec670817a12a2c9905c
Yangshen0325 <sylviadyx77@163.com>
2021-12-17 10:00:57 +0100

    modified anagenesis to make sure it is the species present immigrated that happens to anagentic speciation

--- R/ana_rate.R
+++ R/ana_rate.R
@@ -17,19 +17,33 @@
 # Mt <- {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)}
 # p_status<-c(0,1,1,0)
 # a_status<-c(1,0,0,1,1)
-# #
+# 
+# rep.row<-function(x,n){
+#   matrix(rep(x,each=n),nrow=n)}
+# island_spec_plant <-c("NA","NA","NA","I","NA","NA","NA")
+# island_spec_plant <- rep.row(c(island_spec_plant),4)
+# island_spec_plant[c(1,4),4] <- "A"
+# 
+# island_spec_animal <-c("NA","NA","NA","I","NA","NA","NA")
+# island_spec_animal <- rep.row(c(island_spec_animal),5)
+# island_spec_animal[c(2,3,4),4] <- "A"
+
 # get_ana_rate(laa_par,M0,Mt,p_status,a_status)
 
 get_ana_rate <- function(laa_par,
                          M0,
                          Mt,
                          p_status,
-                         a_status) {  
+                         a_status,
+                         island_spec_plant,
+                         island_spec_animal) {  
   
   plant_ana_rate =  (laa_par[1] + 
-    laa_par[2] *  abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0) %*% a_status[1:NCOL(M0)]) * p_status
+                       laa_par[2] *  abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0) %*% a_status[1:NCOL(M0)]) *
+                        p_status * (island_spec_plant[, 4] == "I")
   animal_ana_rate =  (laa_par[3] + 
-    laa_par[4] * t(abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0)) %*% p_status[1:NROW(M0)]) * a_status
+                        laa_par[4] * t(abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0)) %*% p_status[1:NROW(M0)]) * 
+                          a_status * (island_spec_animal[, 4] == "I")
   
   ana_list <- list(plant_ana_rate = plant_ana_rate,
                    animal_ana_rate = animal_ana_rate)
