8d033b4fedb1dd844237b2fbde0d9b3f0aa87b03
Yangshen0325 <sylviadyx77@163.com>
2021-12-20 08:53:54 +0100

    20/12/2021 modified version (almost done)

--- R/ana_rate.R
+++ R/ana_rate.R
@@ -11,24 +11,21 @@
 # a_status: to show whether the animal species present on island
 #
 # example:
+# island_spec_plant <- matrix(ncol = 7)
+# island_spec_plant[1,] <- c(2,2,0,"I",NA,NA,NA)
+# island_spec_plant <- rbind(island_spec_plant, c(3,3,0,"A",NA,NA,NA))
+# 
+# island_spec_animal <- matrix(ncol = 7)
+# island_spec_animal[1,] <- c(1,1,0,"I",NA,NA,NA)
+# island_spec_animal <- rbind(island_spec_animal,c(5,5,0,"A",NA,NA,NA))
 #
 # laa_par <- c(0.1,0.2,0.1,0.2)
 # M0 <- {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)}
 # Mt <- {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)}
 # p_status<-c(0,1,1,0)
-# a_status<-c(1,0,0,1,1)
+# a_status<-c(1,0,0,0,1)
 # 
-# rep.row<-function(x,n){
-#   matrix(rep(x,each=n),nrow=n)}
-# island_spec_plant <-c("NA","NA","NA","I","NA","NA","NA")
-# island_spec_plant <- rep.row(c(island_spec_plant),4)
-# island_spec_plant[c(1,4),4] <- "A"
-# 
-# island_spec_animal <-c("NA","NA","NA","I","NA","NA","NA")
-# island_spec_animal <- rep.row(c(island_spec_animal),5)
-# island_spec_animal[c(2,3,4),4] <- "A"
-
-# get_ana_rate(laa_par,M0,Mt,p_status,a_status)
+# get_ana_rate(laa_par,M0,Mt,p_status,a_status,island_spec_plant,island_spec_animal)
 
 get_ana_rate <- function(laa_par,
                          M0,
@@ -36,14 +33,21 @@ get_ana_rate <- function(laa_par,
                          p_status,
                          a_status,
                          island_spec_plant,
-                         island_spec_animal) {  
+                         island_spec_animal) { 
+  
+  possible_ana_p <- matrix(0,nrow=NROW(M0))
+  possible_ana_a <- matrix(0,nrow=NCOL(M0))
+  plant_ind <-as.numeric(island_spec_plant[which(island_spec_plant[,4]=="I"),1])
+  animal_ind <-as.numeric(island_spec_animal[which(island_spec_animal[,4]=="I"),1])
+  possible_ana_p[plant_ind] == 1
+  possible_ana_a[animal_ind] == 1
   
   plant_ana_rate =  (laa_par[1] + 
                        laa_par[2] *  abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0) %*% a_status[1:NCOL(M0)]) *
-                        p_status * (island_spec_plant[, 4] == "I")
+    p_status * possible_ana_p
   animal_ana_rate =  (laa_par[3] + 
                         laa_par[4] * t(abs(Mt[1:NROW(M0),1:NCOL(M0)]-M0)) %*% p_status[1:NROW(M0)]) * 
-                          a_status * (island_spec_animal[, 4] == "I")
+    a_status * possible_ana_a
   
   ana_list <- list(plant_ana_rate = plant_ana_rate,
                    animal_ana_rate = animal_ana_rate)
