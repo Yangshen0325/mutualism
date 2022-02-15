7bb0421f830da010f24332b4f82fd8c689949d32
Yangshen0325 <sylviadyx77@163.com>
2022-02-15 08:59:01 +0100

    revised 0214

--- R/simulation_mutualism.R
+++ R/simulation_mutualism.R
@@ -1,80 +1,68 @@
 
-DAISIE_sim_core_mutualism <- function(
-  time=1,
-  mainland_n=1,
-  pars=c(0.5,0.2,Inf,0.05,1),
-  M=100,
-  mutualism_pars = NULL
-){
-  
-  
-  # mutualism_pars <- list(lac_animal,mu_par,K_par,gam_animal,laa_par,qgain,qloss,lambda1)
+DAISIE_sim_core_mutualism <- function(time=1,
+                                      mutualism_pars){
   
   #### Initialization ####
   timeval <- 0
   totaltime <- time
   M0 <- mutualism_pars$M0
-  maxspecID <- 1
-  
   Mt <- M0
+  p_status <- rep(0, NROW(M0))
+  a_status <- rep(0, NCOL(M0))
   
-  island_spec <- c()
+  island_spec_plant <- c()
+  island_spec_animal <- c()
+  
   stt_table <- matrix(ncol = 7)
-  colnames(stt_table) <- c("Time", "nI", "nA", "nC", "nIa","nAa","nCa")
-  
+  colnames(stt_table) <- c("Time", "nIp", "nAp", "nCp", "nIa","nAa","nCa")
   stt_table[1, ] <- c(totaltime, 0, 0, 0, 0, 0, 0)
-
+  
   
   #### Start Monte Carlo iterations ####
   while (timeval < totaltime) {
-    
-    rates <- update_rates_mutual(
-      gam0_par = gam0_par,
-      lac0_par = lac0_par,
-      K_par = K_par,
-      mu_par = mu_par,
-      laa_par = laa_par,
-      lambda1 = lambda1,
-      qloss = qloss,
-      qgain = qgain,
-      M0 = M0,
-      Mt = Mt,
-      p_status = p_status,
-      a_status = a_status,
-      island_spec_plant = island_spec_plant,
-      island_spec_animal = island_spec_animal)
+    rates <- update_rates_mutualism(Mt = Mt,
+                                    p_status = p_status,
+                                    a_status = a_status,
+                                    mutualism_pars = mutualism_pars,
+                                    island_spec_plant = island_spec_plant,
+                                    island_spec_animal = island_spec_animal)
     
     timeval_and_dt <- calc_next_timeval(rates = rates,timeval = timeval)
     timeval <- timeval_and_dt$timeval
     # next time
-    
-    possible_event <- event(rates = rates)
-    # next event
-    
-    updated_state <- update_state(timeval = timeval,
-                                 totaltime = totaltime,
-                                 possible_event = possible_event,
-                                 p=p,
-                                 Mt=Mt,
-                                 p_status=p_status,
-                                 a_status=a_status,
-                                 maxplantID = maxplantID,
-                                 maxanimalID = maxanimalID,
-                                 island_spec_plant = island_spec_plant,
-                                 island_spec_animal = island_spec_animal,
-                                 stt_table = stt_table)
-    
-    Mt <- updated_state$Mt
-    p_status <- updated_state$p_status
-    a_status <- updated_state$a_status
-    island_spec_plant <- updated_state$island_spec_plant
-    island_spec_animal <- updated_state$island_spec_animal
-    maxplantID <- updated_state$maxplantID
-    maxanimalID <- updated_state$maxanimalID
-    stt_table <- updated_state$stt_table
-    #num_spec <- length(island_spec[, 1])
-  }
-  
+    if(timeval <= totaltime){
+      rates <- update_rates_mutualism(Mt = Mt,
+                                      p_status = p_status,
+                                      a_status = a_status,
+                                      mutualism_pars = mutualism_pars)
+      
+      possible_event <- DAISIE_sample_event_mutualism(rates = rates)
+      # next event
+      
+      updated_state <- update_state(timeval = timeval,
+                                    totaltime = totaltime,
+                                    possible_event = possible_event,
+                                    p=p,
+                                    Mt=Mt,
+                                    p_status=p_status,
+                                    a_status=a_status,
+                                    maxplantID = maxplantID,
+                                    maxanimalID = maxanimalID,
+                                    island_spec_plant = island_spec_plant,
+                                    island_spec_animal = island_spec_animal,
+                                    stt_table = stt_table)
+      
+      Mt <- updated_state$Mt
+      p_status <- updated_state$p_status
+      a_status <- updated_state$a_status
+      island_spec_plant <- updated_state$island_spec_plant
+      island_spec_animal <- updated_state$island_spec_animal
+      maxplantID <- updated_state$maxplantID
+      maxanimalID <- updated_state$maxanimalID
+      stt_table <- updated_state$stt_table
+      #num_spec <- length(island_spec[, 1])
+    }
+  } 
   #### Finalize STT ####
   stt_table <- rbind(
     stt_table,
@@ -88,10 +76,9 @@ DAISIE_sim_core_mutualism <- function(
       stt_table[nrow(stt_table), 7]
     )
   )
+  
 }
 
-# save(rates, file="rates.Rdata")
-# save(updated_state, file="updated_state.Rdata")
 
 
 
