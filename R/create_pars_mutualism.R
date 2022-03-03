587c7697e9d7cd6b6f23fab784d3ee4e90c24ccc
Y. Shen <P302656@workspace.rug.nl>
2022-03-03 16:21:26 +0100

    deleted all single rates calculation file and modified create_pars_muutalism.

--- R/create_pars_mutualism.R
+++ R/create_pars_mutualism.R
@@ -1,14 +1,20 @@
 # creat pars
-# mutualism_pars <- list(lac_par,mu_par,K_par,gam_par,laa_par,M0,qgain,qloss,lambda1)
-mutualism_pars <- create_mutualism_pars(lac_par = c(0.1,0.1),
+# mutualism_pars <- list(lac_par,mu_par,K_par,gam_par,laa_par,qgain,qloss,lambda1, M0,)
+# lac_par <- c(lac_plant, lac_animal)
+# mu_par <- c(mu_P0, mu_A0, mu_P1, mu_A1)
+# K_par <- c(K_P0, K_A0, K_P1, K_A1)
+# gam_par <- c(gam_plant, gam_animal)
+# laa_par <- c(laa_P0, laa_A0, laa_P1, laa_A1)
+
+mutualism_pars <- create_mutualism_pars(lac_par = c(0.5,0.5),
                                       mu_par = c(0.2,0.2,0.5,0.5),
                                       K_par = c(Inf,Inf,0.5,0.5),
-                                      gam_par = c(0.3,0.3),
-                                      laa_par = c(0.4,0.4,0.5,0.5),
-M0 <- {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)},
+                                      gam_par = c(0.05, 0),
+                                      laa_par = c(1,1,0.5,0.5),
                                       qgain = 0.5,
                                       qloss = 0.5,
-                                      lambda1 = 0.5)
+                                      lambda1 = 0.5,
+                                      M0 = {set.seed(1);matrix(sample(c(0,1),20,replace = TRUE),ncol=5,nrow=4)})
 
 
 create_mutualism_pars <- function(lac_par,
@@ -16,10 +22,10 @@ create_mutualism_pars <- function(lac_par,
                               K_par,
                               gam_par,
                               laa_par,
-                              M0,
                               qgain,
                               qloss,
-                              lambda1) {
+                              lambda1,
+                              M0) {
   testit::assert(is.numeric(lac_par))
   testit::assert(is.numeric(mu_par))
   testit::assert(is.numeric(K_par))
@@ -42,8 +48,7 @@ create_mutualism_pars <- function(lac_par,
        K_par = K_par,
        gam_par = gam_par,
        laa_par = laa_par,
-       M0 = M0,
        qgain = qgain,
        qloss = qloss,
-       lambda1 = lambda1)
-}
+       lambda1 = lambda1,
+       M0 = M0)}
